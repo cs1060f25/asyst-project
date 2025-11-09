7637d0f2e03775be8f59e63ee7a909f4cfec863d
Brian Lin <brianlin@college.harvard.edu>
2025-11-09 13:05:49 -0500

    HW8 ASYST-18: Fix missing Link import
    - Restore Link import that was accidentally removed during merge
    - Fixes ReferenceError: Link is not defined

--- src/app/candidate/page.tsx
+++ src/app/candidate/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
